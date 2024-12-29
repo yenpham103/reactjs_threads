0ff5c8c4250bea17a4dba8c9ede17d4299585816
yenpx@bsscommerce.com <yenpx@bsscommerce.com>
2024-12-29 16:00:47 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <div>
+      <Button className='bg-red-500'>Click me</Button>
+    </div>
+  )
+}
---
+++ src/routes/privateRoutes.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function privateRoutes() {
+  return (
+    <div>privateRoutes</div>
+  )
+}
